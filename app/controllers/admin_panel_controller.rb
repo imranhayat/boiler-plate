56b8f4fb374fb88d19dffcfa468b4c55987926ec
imranhayat <imran.hayat92@gmail.com>
2019-09-20 17:07:36 +0500

    Admin Panel Mobile view and Topup button, Heaere footer links

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -3,6 +3,7 @@ class AdminPanelController < ApplicationController
   load_and_authorize_resource class: false
   
   def index
+    @users = User.where.not(id: current_user.id)
   end
 
   def users
