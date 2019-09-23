e8060a0da5a27accfacc6ec439c28d2ca66b1ce6
Saqib Shahzad <saqib.lums@gmail.com>
2019-09-23 16:32:02 +0500

    changed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+[User].each do |table|
+  ActiveRecord::Base.connection.execute("TRUNCATE #{table.table_name} RESTART IDENTITY CASCADE")
+end
 User.create(name: 'Saqib', email: "user@test.com",password: "admin123")
 User.create(name: 'Admin', email: "admin@test.com",password: "admin123",admin: true)
