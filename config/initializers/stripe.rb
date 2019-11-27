4bcc5a1809fdea3b228e9ccd5f50e7218cfd2223
mhamzajutt96 <m_hamza_jutt@yahoo.com>
2019-11-27 17:19:21 +0500

    Fixing some Rubocop Errors.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,7 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV["STRIPE_PUBLISHABLE_KEY"],
-  :secret_key      => ENV["STRIPE_SECRET_KEY"],
-  :end_point_secret => ENV["STRIPE_ENDPOINT_SECRET"]
-
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_SECRET_KEY'],
+  :end_point_secret => ENV['STRIPE_ENDPOINT_SECRET']
 }
-
 Stripe.api_key = Rails.configuration.stripe[:secret_key]  
-Stripe.api_version = "2019-03-14"
+Stripe.api_version = '2019-03-14'
