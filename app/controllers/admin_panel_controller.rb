9ca118b39a22011417f2830bdfe41efb765bbec7
mhamzajutt96 <m_hamza_jutt@yahoo.com>
2020-01-10 11:59:03 +0500

    Some Changes in Admin_panel

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -10,16 +10,18 @@ class AdminPanelController < ApplicationController
   end
 
   def users
-    @users = User.where.not(id: current_user.id)
+    @users = User.where.not(id: current_user.id).order('created_at DESC')
   end
 
   def change_access
     @user = User.find(params[:enable] || params[:disable])
     if params[:enable].present?
+      @access = 'Enabled'
       @user.update(revoke_access: false)
     else
+      @access = 'Disabled'
       @user.update(revoke_access: true)
     end
-    redirect_to admin_panel_path
+    redirect_to admin_users_path, notice: "#{@user.email} has been #{@access}"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,8 @@ class User < ApplicationRecord
       user.password = SecureRandom.hex(8)
     end
   end
+
+  def self.normal_users
+    Role.find_by_name('normal').users.count
+  end
 end
