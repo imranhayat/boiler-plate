2a59b22610f455034621ca5997571fc4de48d51a
imranhayat <imran.hayat92@gmail.com>
2021-04-27 12:32:54 +0500

    Separate CSS files according to device widths

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,13 @@ Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( datatables.js datatables.scss script.js swal.js swal.scss dashboard.js dashboard.scss )
+Rails.application.config.assets.precompile += %w(
+
+  application-991.scss application-992.scss application-767.scss application-768.scss application-575.scss application-576.scss 
+
+  signed_in.scss signed_in-991.scss signed_in-992.scss signed_in-767.scss signed_in-768.scss signed_in-575.scss signed_in-576.scss signed_in.js
+
+  dashboard.scss dashboard-992.scss dashboard-767.scss dashboard.js
+
+  datatables.scss datatables.js swal.scss swal.js 
+)
