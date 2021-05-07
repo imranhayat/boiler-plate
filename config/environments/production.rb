2cb47102300ff1b71f0c265d946db3cecbbff411
imranhayat <imran.hayat92@gmail.com>
2021-05-07 22:46:27 +0500

    Production sass compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,15 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+  }
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.read_encrypted_secrets = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
   config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.compress = true
   config.assets.css_compressor = :sass
   config.log_level = :debug
