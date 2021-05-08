82beb647ea3383d01665bbc5dbda5adfb33cae39
Moiz Ali <amoiz4142@gmail.com>
2021-05-08 16:26:34 +0500

    Remove paperclip config and put database.yml into .gitignore

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,12 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
   # User Balance Settings
   monetize :balance_cents, as: 'balance'
-  # Paperclip Settings
-  has_attached_file :profile_pic, styles: { medium: '300x300',
-                                            small: '150x150' },
-                                  default_url: '/avatar.png'
-  validates_attachment_content_type :profile_pic,
-                                    content_type: /\Aimage\/.*\z/
+  # # Paperclip Settings
+  # has_attached_file :profile_pic, styles: { medium: '300x300',
+  #                                           small: '150x150' },
+  #                                 default_url: '/avatar.png'
+  # validates_attachment_content_type :profile_pic,
+  #                                   content_type: /\Aimage\/.*\z/
 
   after_create :assign_default_role
 
